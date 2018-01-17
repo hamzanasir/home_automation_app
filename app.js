1488978b710e507922d90f97804bf61fc273628a
Muhammad Hamza Nasir <mnasir@hawk.iit.edu>
2018-01-17 02:57:34 -0600

    Added started configuration for tp-link devices

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var app = express()
 
+const { Client } = require('tplink-smarthome-api');
+
+const client = new Client();
+const plug = client.getDevice({host: '192.168.0.26'}).then((device)=>{
+  device.getSysInfo().then(console.log);
+  device.setPowerState(false);
+});
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
